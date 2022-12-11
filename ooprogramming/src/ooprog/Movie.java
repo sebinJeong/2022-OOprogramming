85f7ccf38e58a369682c1686d71c4703970649b7
sebinJeong <100884015+sebinJeong@users.noreply.github.com>
2022-12-12 02:54:58 +0900

    Delete ooprogramming directory

--- ooprogramming/src/ooprog/Movie.java
+++
@@ -1,9 +0,0 @@
-package ooprog;
-
-public class Movie extends Hallyu{
-	//super()를 활용하여 부모클래스를 상속받음
-	public Movie(String category, Datatype datatype, String ranking, int ranknum, String country, float percent){
-		super(category, datatype, ranking, ranknum, country, percent);
-	}
-	 
-}
