ef83d5943d34f0b3d2a25cfd4c5f3aa21fafedf3
sebinJeong <100884015+sebinJeong@users.noreply.github.com>
2022-12-12 03:02:25 +0900

    Add files via upload

---
+++ ooprogramming/src/ooprog/Drama.java
@@ -0,0 +1,9 @@
+package ooprog;
+
+public class Drama extends Hallyu{
+	//super()를 활용하여 부모클래스를 상속받음
+	public Drama(String category, Datatype datatype, String ranking, int ranknum, String country, float percent){
+		super(category, datatype, ranking, ranknum, country, percent);
+	}
+	 
+}
